53340125385d21e3ebd72f29f95ca91d8c2f9452
rfr3t <me@example.com>
2020-06-09 21:29:03 -0400

    initial check-in

---
+++ devices/rfdevices.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+import sys
+import rflib
+import bitstring
+
+class RFDevice(object):
+    def _send(self, bin_seq, d):
+        #print bin_seq
+        rf_data = bitstring.BitArray(bin=bin_seq).tobytes()
+        d.setModeTX()
+        d.setFreq(self.frequency)
+        d.setMdmModulation(self.modulation)
+        d.setMdmDRate(self.data_rate)
+        d.setMdmSyncMode(0)
+        d.makePktFLEN(len(rf_data))
+        d.setMaxPower()
+        d.RFxmit(rf_data)
+        d.setModeIDLE()
+
+    def _expand_tribits(self, bits, ZERO='001', ONE='011'):
+        return ''.join(ZERO if b=='0' else ONE for b in bits)
+
+class Fireplace(RFDevice):
+    def __init__(self):
+        self.frequency = 303870000
+        self.modulation = rflib.MOD_ASK_OOK
+        self.data_rate = int(1/.00058175)
+        self.prefix = '00000001'
+
+    def turnon(self, dongle): 
+        on  = '010001101001010001111000'
+        bin_seq = (self.prefix + self._expand_tribits(on))*10
+        self._send(bin_seq, dongle)
+
+    def turnoff(self, dongle):
+        off = '010001101001010001101001'
+        bin_seq = (self.prefix + self._expand_tribits(off))*10
+        self._send(bin_seq, dongle)
+   
+class HunterFanLight(RFDevice):
+    def __init__(self):
+        self.frequency = 433900000
+        self.modulation = rflib.MOD_ASK_OOK
+        self.data_rate = int(1/0.0004) #0.4ms
+        self.prefix = '0'*62 + '01'*12 + '0'*13
+        self.code = None
+
+    def toggle_light(self, dongle):
+        light_pt1  = self.code + '10001000000001110111111110'
+        light_pt2  = self.code + '10000001001101111110110010'
+        bin_seq = 3*(self.prefix + self._expand_tribits(light_pt1, ZERO='100', ONE='110')) + \
+                  2*(self.prefix+self._expand_tribits(light_pt2, ZERO='100', ONE='110'))  
+        self._send(bin_seq, dongle)
+
+    def turn_light_off(self, dongle):
+        self.toggle_light(dongle)
+
+    def turn_light_on(self, dongle):
+        self.toggle_light(dongle)
+
+    def set_fan_low(self, dongle):
+        fanlow = self.code + '10000000000101111111111010'
+        bin_seq = 4*(self.prefix + self._expand_tribits(fanlow, ZERO='100', ONE='110'))
+        self._send(bin_seq, dongle)
+
+    def set_fan_medium(self, dongle):
+        fanmed = self.code + '10000001000001111110111110'
+        bin_seq = 4*(self.prefix + self._expand_tribits(fanmed, ZERO='100', ONE='110'))
+        self._send(bin_seq, dongle)
+
+    def set_fan_high(self, dongle):
+        fanhigh = self.code + '10000010000001111101111110'
+        bin_seq = 4*(self.prefix + self._expand_tribits(fanhigh, ZERO='100', ONE='110'))
+        self._send(bin_seq, dongle)
+
+    def turn_fan_off(self, dongle):
+        fanoff_pt1 = self.code + '10000000000011111111111100'
+        fanoff_pt2 = self.code + '10000001000101111110111010'
+        bin_seq = 3*(self.prefix + self._expand_tribits(fanoff_pt1, ZERO='100', ONE='110')) + 2*(self.prefix + self._expand_tribits(fanoff_pt2, ZERO='100', ONE='110'))
+        self._send(bin_seq, dongle)
+
+class Bedroom2FanLight(HunterFanLight):
+    def __init__(self):
+        super(Bedroom2FanLight, self).__init__()
+        self.code = '11010001 11100011 00000011 01010100 10100000'.replace(" ", "")
+
+
+class Bedroom1FanLight(HunterFanLight):
+    def __init__(self):
+        super(Bedroom1FanLight, self).__init__()
+        self.code = '10111001 00110101 11101110 11011101 01000011'.replace(" ", "")
+
---
+++ homecontrol.py
@@ -0,0 +1,65 @@
+import rflib
+from bottle import route, run, abort
+import devices.rfdevices as rf
+
+d=rflib.RfCat()
+
+@route('/fireplace/<action>')
+def control_fireplace(action):
+    if action == 'on':
+        rf.Fireplace().turnon(d)
+    elif action == 'off':
+        rf.Fireplace().turnoff(d)
+    else:
+        abort(404, "Not Found")
+    return "ok"
+
+@route('/bedroom-2-light/<action>')
+def control_light(action):
+    if action == 'on':
+        rf.Bedroom2FanLight().turn_light_on(d)
+    elif action == "off":
+        rf.Bedroom2FanLight().turn_light_off(d)
+    else:
+        abort(404, "Not Found")
+    return "ok"
+
+@route('/bedroom-2-fan/<action>')
+def control_fan(action):
+    if action == 'off':
+        rf.Bedroom2FanLight().turn_fan_off(d)
+    elif action == 'low':
+        rf.Bedroom2FanLight().set_fan_low(d)
+    elif action == 'med':
+        rf.Bedroom2FanLight().set_fan_medium(d)
+    elif action == 'high':
+        rf.Bedroom2FanLight().set_fan_high(d)
+    else: 
+        abort(404, "Not Found")
+    return "ok"
+
+@route('/bedroom-1-light/<action>')
+def control_light(action):
+    if action == 'on':
+        rf.Bedroom1FanLight().turn_light_on(d)
+    elif action == "off":
+        rf.Bedroom1FanLight().turn_light_off(d)
+    else:
+        abort(404, "Not Found")
+    return "ok"
+
+@route('/bedroom-1-fan/<action>')
+def control_fan(action):
+    if action == 'off':
+        rf.Bedroom1FanLight().turn_fan_off(d)
+    elif action == 'low':
+        rf.Bedroom1FanLight().set_fan_low(d)
+    elif action == 'med':
+        rf.Bedroom1FanLight().set_fan_medium(d)
+    elif action == 'high':
+        rf.Bedroom1FanLight().set_fan_high(d)
+    else: 
+        abort(404, "Not Found")
+    return "ok"
+
+run(host='0.0.0.0', port=80)
